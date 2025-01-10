5f8ce52fe2a0bf461c2753b1bb64c86a33482adc
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-01-10 16:46:40 +0900

    event/latest/ 추가

--- event/views.py
+++ event/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Event
 
 def latest(request):
-    return 0
+    latest_event=Event.objects.order_by('-create_date').first()
+    if latest_event:
+        return JsonResponse({
+            'id': latest_event.id,
+            'title': latest_event.title,
+            'create_date': latest_event.create_date,
+            'start_date': latest_event.start_date,
+            'end_date': latest_event.end_date,
+            'location': latest_event.location,
+            'place': latest_event.place,
+            'leagues': latest_event.leagues,
+            'poster_top': latest_event.poster_top.url if latest_event.poster_top else None,
+            'poster_bottom': latest_event.poster_bottom.url if latest_event.poster_bottom else None,
+        })
+    else:
+        # Return an empty response if no event exists
+        return JsonResponse({'error': 'No events found'}, status=404)
