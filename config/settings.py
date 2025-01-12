629f1b623c00b9df0b87acb17a4349a189a75620
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-01-12 20:31:14 +0900

    csrf 설정추가

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://port-0-rwdp-backend-m5swwza017d4d90c.sel4.cloudtype.app"
+]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
