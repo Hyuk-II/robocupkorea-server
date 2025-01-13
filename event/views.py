9568fe6a54cab238cd64ffd3746d64f1eea590cf
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-01-13 19:40:46 +0900

    request 추가

--- event/views.py
+++ event/views.py
@@ -25,9 +25,9 @@ def latest(request):
             "dates": [latest_event.start_date, latest_event.end_date],
             "location": latest_event.location,
         }
-        return JsonResponse(result)
+        return JsonResponse(request, result)
     else:
-        return JsonResponse({"error": "No events found"}, status=404)
+        return JsonResponse(request, {"error": "No events found"}, status=404)
 
 
 def event_info(request, event_id):
