10ed4be9064d3a65f744706d9472875ce6994647
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-01-21 14:34:26 +0900

    Style: 코드 최적화

--- event/models.py
+++ event/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from config.supabase_storage import SupabaseStorage
 
 
-class ETC(models.Model):  # 관련 페이지
+class ETC(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
 
@@ -15,20 +15,16 @@ class ETC(models.Model):  # 관련 페이지
 
 class Event(models.Model):
     id = models.CharField(max_length=50, primary_key=True, null=False)
-    title = models.CharField(max_length=50)  # 대회제목
+    title = models.CharField(max_length=50)
 
-    # 개최기간
     start_date = models.DateField()
     end_date = models.DateField()
 
-    # 개최장소 이름, 구글맵 링크
     location = models.CharField(max_length=50)
     map = models.CharField(max_length=255)
 
-    # 관련 내용, etc 모델의 id를 참조
     others = models.ManyToManyField(ETC, blank=True)
 
-    # 포스터 사진
     image_top = models.ImageField(
         storage=SupabaseStorage(), upload_to="posters/top/", blank=True
     )
@@ -36,7 +32,6 @@ class Event(models.Model):
         storage=SupabaseStorage(), upload_to="posters/bottom/", blank=True
     )
 
-    # 개최리그
     leagues = ArrayField(models.CharField(max_length=50), blank=True, default=list)
 
     def __str__(self):
