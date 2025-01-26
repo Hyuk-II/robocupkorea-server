652834145c8626169bd95446f0ef30e63364fc39
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-01-26 20:45:42 +0900

    feat: admin eng model 관리 추가, notice 검색 추가

--- league/admin.py
+++ league/admin.py
@@ -10,5 +10,15 @@ class AttachAdmin(admin.ModelAdmin):
     exclude = ("name", "size")
 
 
+class LeagueENGAdmin(admin.ModelAdmin):
+    search_fields = ["id"]
+
+
+class AttachENGAdmin(admin.ModelAdmin):
+    exclude = ("name", "size")
+
+
 admin.site.register(League, LeagueAdmin)
 admin.site.register(Attachment, AttachAdmin)
+admin.site.register(League_ENG, LeagueENGAdmin)
+admin.site.register(Attachment_ENG, AttachENGAdmin)
--- notice/admin.py
+++ notice/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from .models import *
 
 
+class NoticeAdmin(admin.ModelAdmin):
+    search_fields = ["id"]
+
+
 class AttachAdmin(admin.ModelAdmin):
     exclude = ("name", "size")
 
 
-admin.site.register(Notice)
+class NoticeENGAdmin(admin.ModelAdmin):
+    search_fields = ["id"]
+
+
+class AttachENGAdmin(admin.ModelAdmin):
+    exclude = ("name", "size")
+
+
+admin.site.register(Notice, NoticeAdmin)
 admin.site.register(Attachment, AttachAdmin)
+admin.site.register(Notice_ENG, NoticeENGAdmin)
+admin.site.register(Attachment_ENG, AttachENGAdmin)
