8fa6730232df527b9036552637efd9d5f1ad5307
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-01-31 17:16:39 +0900

    Refator: admin 최적화

--- notice/admin.py
+++ notice/admin.py
@@ -3,7 +3,6 @@ from .models import *
 
 
 class NoticeAdmin(admin.ModelAdmin):
-<<<<<<< HEAD
     list_display = ("id",)
     readonly_fields = ("id",)
     fields = ("id", "attachments")
@@ -19,24 +18,3 @@ class NoticeENGAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Notice_ENG, NoticeENGAdmin)
-=======
-    search_fields = ["id"]
-
-
-class AttachAdmin(admin.ModelAdmin):
-    exclude = ("name", "size")
-
-
-class NoticeENGAdmin(admin.ModelAdmin):
-    search_fields = ["id"]
-
-
-class AttachENGAdmin(admin.ModelAdmin):
-    exclude = ("name", "size")
-
-
-admin.site.register(Notice, NoticeAdmin)
-admin.site.register(Attachment, AttachAdmin)
-admin.site.register(Notice_ENG, NoticeENGAdmin)
-admin.site.register(Attachment_ENG, AttachENGAdmin)
->>>>>>> 652834145c8626169bd95446f0ef30e63364fc39
